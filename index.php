a1a4503d412695a5896c8ecbe7ef2ae9a575caa8
lloyddominic26 <ademic26@yahoo.com>
2017-01-03 01:24:51 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+<title>lloyddominic26</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<link rel="stylesheet" type="text/css" href="/css/Material Design/Material Design.css">
+<style>
+/* Theme Colors */
+.bg-theme { background: #4CAF50 !important; }
+.bg-theme.light-1 { background: #5DBF61 !important; }
+.bg-theme.light-2 { background: #6ECF72 !important; }
+.bg-theme.dark-1 { background: #3B9E40 !important; }
+.bg-theme.dark-2 { background: #2A8D30 !important; }
+.bg-text { color: #4CAF50; }
+
+.navbar .navbar-nav > li > a { color: #4CAF50; }
+#sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+}
+.navbar .link {
+    display: inline-block;
+    width: 80%;
+}
+.navbar .fa-caret-down {
+    display: inline-block;
+    width: 10%;
+    text-align: right;
+}
+</style>
+</head>
+
+<body>
+<div class="row no-margin">
+<div class="col-3 bg-white" id="sidebar">
+<header class="bg-theme">
+<div class="padding-lg bg-theme dark-1 text-white">User</div>
+</header>
+<nav class="navbar nav-static-left">
+<div class="container-fluid no-padding">
+<div id="navbar">
+<ul class="navbar-nav">
+<li>
+<a>
+<span class="fa fa-home fa-fw"></span>
+<span class="link">Home</span>
+<span class="fa fa-caret-down fa-fw"></span>
+</a>
+</li>
+<li>
+<a>
+<span class="fa fa-user fa-fw"></span>
+<span class="link">About</span>
+<span class="fa fa-caret-down fa-fw"></span>
+</a>
+</li>
+<li>
+<a>
+<span class="fa fa-book fa-fw"></span>
+<span class="link">Blog</span>
+</a>
+</li>
+<li>
+<a>
+<span class="link">¯\_(ツ)_/¯</span>
+</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+</div>
+</div>
+</body>
+
+</html>
